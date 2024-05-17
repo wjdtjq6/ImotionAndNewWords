8d48bdae15e9b4f21c985441c095d29819f0d924
jimmy <wjdtjq6@gmail.com>
2024-05-17 16:08:49 +0900

    Initial Commit

---
+++ ImotionAndNewWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImotionAndNewWords
+//
+//  Created by t2023-m0032 on 5/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
