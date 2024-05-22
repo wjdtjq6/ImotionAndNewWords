664d258a3012419e7f035279458b3fdfcf61ac93
jimmy <wjdtjq6@gmail.com>
2024-05-22 21:05:33 +0900

    Added UserDefault in EmotionApp

--- ImotionAndNewWords/Assignment/ImotionViewController.swift
+++ ImotionAndNewWords/Assignment/ImotionViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ImotionViewController: UIViewController {
 
+    @IBOutlet var resetButton: UIButton!
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var topImage: UIImageView!
   
@@ -17,7 +18,7 @@ class ImotionViewController: UIViewController {
     @IBOutlet var countLabels: [UILabel]!
     
     var counts = [Int.random(in: 0...100),Int.random(in: 0...100),Int.random(in: 0...100),Int.random(in: 0...100),Int.random(in: 0...100),Int.random(in: 0...100),Int.random(in: 0...100),Int.random(in: 0...100),Int.random(in: 0...100)]
-
+    
     var texts = ["사랑해", "좋아해", "행복해", "서운해", "우울해", "공허해", "해피해" ,"러브해", "도전해"]
     
     override func viewDidLoad() {
@@ -29,10 +30,16 @@ class ImotionViewController: UIViewController {
         titleLabel.textAlignment = .center
         titleLabel.font = .boldSystemFont(ofSize: 20)
         
+        resetButton.setTitle("Reset", for: .normal)
+        resetButton.titleLabel?.textAlignment = .right
+        
         for i in 0...buttons.count-1 {
             buttons[i].setImage(UIImage(named: "slime\(i+1)")?.withRenderingMode(.alwaysOriginal), for: .normal)
         }
         for i in 0...countLabels.count-1 {
+            if UserDefaults.standard.string(forKey: "\(i)") != nil {
+                counts[i] = Int(UserDefaults.standard.string(forKey: "\(i)")!)!
+            }
             countLabels[i].text = "\(texts[i]) \(counts[i])"
             countLabels[i].textAlignment = .center
         }
@@ -40,7 +47,15 @@ class ImotionViewController: UIViewController {
     
     @IBAction func buttonsPressed(_ sender: UIButton) {
         counts[sender.tag] += 1
-        countLabels[sender.tag].text = "\(texts[sender.tag]) \(counts[sender.tag])"
-//        print(sender.tag)
+        UserDefaults.standard.set(counts[sender.tag], forKey: "\(sender.tag)")
+        countLabels[sender.tag].text = "\(texts[sender.tag]) \(UserDefaults.standard.string(forKey: "\(sender.tag)")!)"
+    }
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        for i in 0...counts.count-1 {
+            UserDefaults.standard.set(0, forKey: "\(i)")
+            counts[i] = 0
+            countLabels[i].text = "\(texts[i]) \(counts[i])"
+            
+        }
     }
 }
